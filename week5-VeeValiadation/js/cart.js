aa4c627f349019c3c5d086f48233218f1e8aa6a8
Celeste6666 <aafree2014@gmail.com>
2020-07-27 16:30:20 +0800

    新增第五週訊息回饋中文語系

--- week5-VeeValiadation/js/cart.js
+++ week5-VeeValiadation/js/cart.js
@@ -1,6 +1,7 @@
 const apiPath = 'https://course-ec-api.hexschool.io/api/';
 const uuid = '8ef975e4-f1b6-4328-932c-fe911c580ec7';
 import list from "./component_cart/component_list.js";
+import zh_TW from './component_cart/zh_TW.js';
 // 將 VeeValidate input 驗證工具載入 作為全域註冊
 Vue.component('validation-provider', VeeValidate.ValidationProvider);
 Vue.component('loading', VueLoading);
@@ -13,6 +14,8 @@ VeeValidate.configure({
     invalid: 'is-invalid',
   }
 });
+// 加入至 VeeValidate 的設定檔案
+VeeValidate.localize('tw', zh_TW);
 Vue.component('list', list);
 new Vue({
   el: '#vm',
